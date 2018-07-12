57578f23efc8162d48c2bf0eb58b289dd267d87a
Jeffrey Wang <jeffrey868@gmail.com>
2018-07-12 10:43:49 -0400

    add multiple effects

--- syde361MFP/syde361MFP.ino
+++ syde361MFP/syde361MFP.ino
@@ -10,7 +10,7 @@ const byte channelPin = A4;
 const byte repeatNotePin = A2;
 
 int currentChannel = 1;
-bool repeat = false;
+int currentEffect = 0;
 int repeatTimes = 5;
 
 int velocity = 100;
@@ -22,14 +22,12 @@ void setup() {
   Serial.begin(9600);
   
   attachInterrupt(digitalPinToInterrupt(channelPin), changeChannel, RISING);
-  attachInterrupt(digitalPinToInterrupt(repeatNotePin), toggleRepeat, RISING);
-
-  
+  attachInterrupt(digitalPinToInterrupt(repeatNotePin), changeEffect, RISING);
 }
 
 void sendNote(int midiNote) {
   usbMIDI.sendNoteOn(midiNote, velocity, currentChannel);
-  delay(100);
+  delay(150);
   usbMIDI.sendNoteOff(midiNote, velocity, currentChannel);
 }
 
@@ -47,31 +45,31 @@ void loop() {
   inputVoltage1 = analogRead(interruptPin1) * 0.0049;
   inputVoltage2 = analogRead(interruptPin2) * 0.0049;
   inputVoltage3 = analogRead(interruptPin3) * 0.0049;
-  
+
   if (inputVoltage1 > 3) {
-    if (!repeat) {
+    if (currentEffect <= 1) {
        sendNote(40);
-    } else {
+    } else if (currentEffect == 2) {
        sendRepeatedNote(40);
-    }
+    } 
     inputVoltage1 = 0;
   }
 
   if (inputVoltage2 > 3) {
-    if (!repeat) {
+    if (currentEffect <= 1) {
        sendNote(45);
-    } else {
+    } else if (currentEffect == 2) {
        sendRepeatedNote(45);
-    }
+    } 
     inputVoltage2 = 0;
   }
 
   if (inputVoltage3 > 3) {
-    if (!repeat) {
+    if (currentEffect <= 1) {
        sendNote(50);
-    } else {
+    } else if (currentEffect == 2) {
        sendRepeatedNote(50);
-    }
+    } 
     inputVoltage3 = 0;
   }
 }
@@ -84,11 +82,19 @@ void changeChannel() {
   }
 }
 
-void toggleRepeat() {
+void changeEffect() {
   int repeatPinVoltage = analogRead(repeatNotePin) * 0.0049;
-
+   
   if (repeatPinVoltage > 3) {
-    repeat = !repeat;
+    if (currentEffect == 0) {
+      currentEffect = 1;
+      usbMIDI.sendControlChange(2, 60, currentChannel);
+    } else if (currentEffect == 1) {
+      currentEffect = 2;
+      usbMIDI.sendControlChange(2, 25, currentChannel);
+    } else if (currentEffect == 2) {
+      currentEffect = 0;
+    } 
   }
 }
 
